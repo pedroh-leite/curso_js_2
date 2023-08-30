2100bab3746d4a2521bdf721f043d48e3e1d2265
pedroh-leite <pedrohleite.alves@gmail.com>
2023-08-30 15:26:43 -0300

    Arrumando o ultimo ex

--- 5_ex_programacao/ex10/script.js
+++ 5_ex_programacao/ex10/script.js
@@ -1,6 +1,6 @@
 // Exercício 10
 
-/*let n = 23;
+let n = 23;
 let divisoes = 0;
 
 for(let i = 1; i <= n; i++){
@@ -13,18 +13,6 @@ if(divisoes == 2){
     console.log(`O número ${n} é primo!`);
 } else{
     console.log(`O número ${n} não é primo!`);
-} */
+} 
 
-function classificarNumero(n){
-    if(n > 0 && n % 2 == 0) {
-        console.log("Positivo e Par")
-    } else if(n > 0 && n % 2 == 1){
-        console.log("Positivo e ímpar")
-    } else if(n < 0) {
-        console.log("Negativo")
-    } else if(n == 0){
-        console.log("Neutro")
-    }
-};
 
-classificarNumero(15);
